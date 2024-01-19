ea5b38c003ea7cc09d63c0e082cf189552a58a7b
Daniel Felix <felixdaniel2056@gmail.com>
2024-01-19 02:57:22 +0100

    chore: switching to build from production

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,6 @@ export default defineConfig({
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
-            '@bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
         },
     },
 });
